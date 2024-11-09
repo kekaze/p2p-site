f8542c86daa9d42f437692677d87fa5a7e1b1e91
kekaze <jroyceyanson@gmail.com>
2024-11-09 13:02:08 +0800

    :art: added model for displaying a single request

--- P2PSite/Models/PurchaseRequest.cs
+++ P2PSite/Models/PurchaseRequest.cs
@@ -8,4 +8,23 @@
         public string Status { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
     }
+
+    public class OnePurchaseRequest
+    {
+        public int PurchaseId { get; set; }
+        public string Purpose { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public string CompanyId { get; set; } = string.Empty;
+        public string CompanyName { get; set;} = string.Empty;
+        public Material[] Materials { get; set; }
+    }
+
+    public class Material
+    {
+        public int MaterialCode { get; set; }
+        public string MaterialName { get; set; }
+        public float Quantity { get; set; }
+    }
 }
